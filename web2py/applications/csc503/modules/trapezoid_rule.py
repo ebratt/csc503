8f80e3bab003f95b62d5e3251aa26cdf6d6732c9
ebratt <eric_bratt@yahoo.com>
2015-03-01 02:27:35 -0600

    add a pyopencl algorithm add examples of input for each algorithm

--- web2py/applications/csc503/modules/trapezoid_rule.py
+++ web2py/applications/csc503/modules/trapezoid_rule.py
@@ -1,129 +1,99 @@
-"""
-Created by Eric Bratt, 2015
+# -*- coding: utf-8 -*-
 
-Example taken from Massimo Di Pierro, DePaul University
-"""
-
+import random
 import logging
 import timeit as ti
 import signal
-import sys
-from random import random
-
-from nlib import Canvas
-
-import plot as plt
 from psim import *
 import log
+import sys
+import plot as plt
 import utility
+import math
+from nlib import Canvas
+from numpy import arange
 
-
+# TODO: accept all variables as input rather than hard-coding them
 input_data = None
 topology = SWITCH
 bases = []
 procs_list = ['serial', '2', '4', '8', '16']
 logger = None
 debug = False
-# TODO: accept all variables as input rather than hard-coding them
-# input domain
-a = None  # time zero
-b = None  # time 1 second
-# input Dirichelet boundary conditions
-fa = None  # meters
-fb = None  # 1 meter after b=1 seconds
-
-# input physical parameter
-alpha = None  # g/mass/2
-
-# simulation parameters
-n = None  # this is the number of points used to estimate
-h = None
 
 
-def rules(Aip, Ai, Aim):
-    return (Aip + Aim) / 2.0 + alpha * h * h
+def f(x):
+    """
+    input_data[0] must evaluate to a valid python math function;
+        ex: math.sin(x)+5, 0.0, 3.0, 1024
+    :param x:
+    :return:
+    """
+    return eval(input_data[0])
 
 
-def evolve(A):
-    n = len(A)
-    B = [0 for k in range(n)]
-    for i in range(1, n - 1):
-        B[i] = rules(A[i - 1], A[i], A[i + 1])
-    return B
+def trap(left_endpt, right_endpt, trap_count, base_len):
+    estimate = 0.0
+    x = 0.0
+    i = 0
+
+    estimate = (f(left_endpt) + f(right_endpt))/2.0
+    for i in range(1, trap_count):
+        x = left_endpt + i*base_len
+        estimate += f(x)
+        logger.log_a_value('step %d: estimate for f(%d) is: %f' % (i, x, estimate*base_len), True)
+    estimate = estimate*base_len
+    return estimate
 
 
-def plot(B):
-    points = [(a + i * h, xi) for i, xi in enumerate(B)]
-    canvas.plot(points).save(trajectorypngfilename)
-
-
-def serial_print(A):
-    B = A[1:-1]
-    logger.log_a_value('B is: %s' % B, debug)
-    plot(B)
+def plot():
+    points = [(x, f(x)) for x in arange(0.0, float(input_data[2])+1.0, 0.1)]
+    canvas.plot(points).save(uploadpngfilename)
 
 
 def run_serial():
+    a = float(input_data[1])
+    b = float(input_data[2])
+    n = int(input_data[3])
+    h = (b - a) / n
     logger.setup('serial', input_data)
-    A = [random.random() for k in range(n)]
-    logger.log_a_value('A: %s' % A, debug)
-
-    for t in range(200):
-        A = evolve(A)
-        A[1] = fa
-        A[-2] = fb
-        if t % 10 == 0:
-            serial_print(A)
-        t += 1
-    logger.log_a_value('A evolved is: %s' % A, debug)
-
-
-def parallel_print(comm, A):
-    B = A[1:-1]
-    B = comm.all2one_collect(0, B)
-    if comm.rank == 0:
-        logger.log_a_value('B is: %s' % B, debug)
+    estimate = trap(a, b, n, h)
+    logger.log_a_value('With n = %d trapezoids, our estimate' % n, debug)
+    logger.log_a_value('of the integral from %d to %d = %f' % (a, b, estimate), debug)
+    logger.log_a_value('of the integral from %d to %d = %f' % (a, b, estimate), True)
+    return
 
 
 def run_parallel(p):
+    a = float(input_data[1])
+    b = float(input_data[2])
+    n = int(input_data[3])
+    total_int = 0.0
+    source = 0
     comm = PSim(p, topology, logger)
-    root = 0
-    if comm.rank == root:
+    h = (b - a) / n         # h is the same for all processes
+    local_n = n / p         # local_n is the number of trapezoids
+
+    local_a = a + comm.rank*local_n*h
+    local_b = local_a + local_n*h
+    local_int = trap(local_a, local_b, local_n, h)
+
+    if comm.rank == 0:
         logger.setup('parallel (%s)' % p, input_data)
-        A = [random.random() for k in range(n)]
-        logger.log_a_value('A is: %s' % A, debug)
+        logger.log_a_value('# processes       : %d' % p, debug)
+        logger.log_a_value('# processes       : %d' % p, True)
+    total_int = comm.all2one_reduce(0, local_int, comm.sum)
+    if comm.rank == 0:
+        logger.log_a_value('With n = %d trapezoids, our estimate' % n, debug)
+        logger.log_a_value('With n = %d trapezoids, our estimate' % n, True)
+        logger.log_a_value('of the integral from %d to %d = %f' % (a, b, total_int), debug)
+        logger.log_a_value('of the integral from %d to %d = %f' % (a, b, total_int), True)
     else:
-        A = None
-
-    A = comm.one2all_scatter(root, A)
-    A = [0] + A + [0]
-    right = (comm.rank + 1) % comm.nprocs
-    left = (comm.rank - 1 + comm.nprocs) % comm.nprocs
-
-    for t in range(200):
-        comm.send(right, A[-2])
-        A[0] = comm.recv(left)
-        comm.send(left, A[1])
-        A[-1] = comm.recv(right)
-
-        A = evolve(A)
-
-        # impose the boundary conditions
-        if comm.rank == 0:
-            A[1] = fa
-        if comm.rank == p - 1:
-            A[-2] = fb
-
-        if t % 10 == 0:
-            parallel_print(comm, A)
-        t += 1
-    if comm.rank != root:
         os.kill(comm.pid, signal.SIGTERM)
+    return
 
 
 if __name__ == "__main__":
-    # create a canvas to plot the trajectory
-    canvas = Canvas()
     # check the command-line args
     api_url, \
     download_url, \
@@ -135,11 +105,12 @@ if __name__ == "__main__":
 
     # make the get calls
     input_data, auth = utility.get_data(api_url, download_url, simulation_id)
-    input_data = int(input_data[0])
+
     # setup the files
-    logfile, pngfilename, trajectorypngfilename = \
+    logfile, pngfilename, uploadpngfilename = \
         utility.setup_files(simulation_id, owner_id, session_id, algorithm_name)
-    # setup the logger
+
+   # setup the logger
     if log_level == 'INFO':
         log_level = logging.INFO
     if log_level == 'DEBUG':
@@ -147,20 +118,10 @@ if __name__ == "__main__":
     logger = log.psim2web2pyLogger('root', logfile, log_level)
     logger.log_system_info(algorithm_name)
     logger.log_a_value('main: START', debug)
-    # input domain
-    a = 0.0  # time zero
-    b = 1.0  # time 1 second
-    # input Dirichelet boundary conditions
-    fa = 0.0  # meters
-    fb = 1.0  # 1 meter after b=1 seconds
+    logger.log_a_value('main: START', True)
 
-    # input physical parameter
-    alpha = 5.0  # g/mass/2
-
-    # simulation parameters
-    # p = int(sys.argv[1])
-    n = input_data  # this is the number of points used to estimate
-    h = (b - a) / n
+    canvas = Canvas(title='Plot of f(x) = %s' % input_data[0], ylab='f(x)')
+    plot()
 
     # serial
     bases.append(ti.timeit(stmt='run_serial()',
@@ -190,7 +151,7 @@ if __name__ == "__main__":
     # respectively.
     log_files = {'log_content': open(logfile, 'rb')}
     plot_files = {'plot_content': open(pngfilename, 'rb')}
-    upload_files = {'upload_content': open(trajectorypngfilename, 'rb')}
+    upload_files = {'upload_content': open(uploadpngfilename, 'rb')}
     log_payload = {'simulation': simulation_id, 'log_owner': owner_id}
     plot_payload = {'simulation': simulation_id, 'plot_owner': owner_id}
     upload_payload = {'simulation': simulation_id, 'upload_owner': owner_id}
@@ -202,7 +163,7 @@ if __name__ == "__main__":
         utility.make_requests(api_url, auth, log_files, plot_files, upload_files,
                               log_payload, plot_payload, upload_payload)
     # cleanup temp files
-    utility.remove_files(logfile, pngfilename, trajectorypngfilename)
+    utility.remove_files(logfile, pngfilename, uploadpngfilename)
 
 __author__ = 'Eric Bratt'
 __version__ = 'version 1.0'
